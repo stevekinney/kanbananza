d5bf77348a1474605815c71d6a0dd3fb1503e29d
Steve Kinney <hello@stevekinney.net>
2019-05-29 14:23:59 -0500

    Implement reselect

--- src/containers/MoveCardToListContainer.js
+++ src/containers/MoveCardToListContainer.js
@@ -1,9 +1,20 @@
 import { connect } from 'react-redux';
 import { moveCardToList } from '../actions/card-actions';
 import MoveCardToList from '../components/MoveCardToList';
+import { createSelector } from 'reselect';
+
+const getListEntities = state => state.lists.entities;
+
+const getLists = createSelector(
+  [getListEntities],
+  lists => {
+    console.log('Generating an array of the lists');
+    return Object.values(lists);
+  },
+);
 
 const mapStateToProps = state => {
-  return { lists: state.lists.entities };
+  return { lists: getLists(state) };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
